d2bd442a6f91a1aacf601233ddd357a30e5347b8
Rajesh Paudel <rajeshpaudel9863@gmail.com>
2026-06-30 16:53:28 +0545

    changes

--- index.js
+++ index.js
@@ -22,7 +22,12 @@ const PORT = process.env.PORT || 5000;
 dbConnect();
 
 // Global Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://fe-tutor-rho.vercel.app/",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 // Bind API Application Routes
